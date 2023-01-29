c7d3e46ad9a0cd380e78b255b755ce4aaecb4aee
Mary Ball <marusaball@yandex.ru>
2023-01-29 18:11:21 +0300

    test 14 sprint

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const Constants = require('../utils/constants');
+
 const {
   getUsers,
   getUser,
@@ -7,9 +10,27 @@ const {
 } = require('../controllers/users');
 
 router.get('/', getUsers);
-router.get('/:userId', getUser);
+router.get(
+  '/:userId',
+  celebrate({
+    params: Joi.object().keys({
+      id: Joi.string().pattern(/^[0-9a-fA-F]{24}$/),
+    }),
+  }),
+  getUser,
+);
 
-router.patch('/me', updateProfile);
-router.patch('/me/avatar', updateAvatar);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateProfile);
+
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().min(2).pattern(Constants.REGEXPHTTP),
+  }),
+}), updateAvatar);
 
 module.exports = router;

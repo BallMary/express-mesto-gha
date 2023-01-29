253c8626b8d3b587462a83a2b4dbd3cdc2ca7d01
Mary Ball <marusaball@yandex.ru>
2023-01-29 18:19:00 +0300

    sprint 14 test

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const Constants = require('../utils/constants');
+
 const {
   getCards,
   createCard,
@@ -8,9 +11,26 @@ const {
 } = require('../controllers/cards');
 
 router.get('/', getCards);
-router.post('/', createCard);
-router.delete('/:cardId', deleteCard);
-router.put('/:cardId/likes', likeCard);
-router.delete('/:cardId/likes', dislikeCard);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().min(2).pattern(Constants.REGEXPHTTP),
+  }),
+}), createCard);
+router.delete('/:cardId', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().pattern(/^[0-9a-fA-F]{24}$/),
+  }),
+}), deleteCard);
+router.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().pattern(/^[0-9a-fA-F]{24}$/),
+  }),
+}), likeCard);
+router.delete('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().pattern(/^[0-9a-fA-F]{24}$/),
+  }),
+}), dislikeCard);
 
 module.exports = router;
